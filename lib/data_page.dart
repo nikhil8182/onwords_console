e119eab5eefc6854de720ea4f0716f2c022cc7d1
parthosh-onwords <parthosh@onwords.in>
2022-06-22 14:07:47 +0530

    main workchances on data

--- lib/data_page.dart
+++ lib/data_page.dart
@@ -3,11 +3,9 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-
 FirebaseAuth auth = FirebaseAuth.instance;
 final databaseReference = FirebaseDatabase.instance.reference().child('staff');
 // final staffDatabaseReference = FirebaseDatabase.instance.reference().child('staff');
-
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -27,7 +25,7 @@ class _MyAppState extends State<MyApp> {
   List time_in_hours = [];
   List workDone = [];
   List workPrecentage = [];
-  var nik="";
+  var nik = "";
   var wrkManage;
   String formattedDate = " ";
   var formatter = DateFormat('yyyy-MM-dd');
@@ -37,28 +35,33 @@ class _MyAppState extends State<MyApp> {
   String combination = " ";
   String dumTime = " ";
   var data;
+  var personalData;
   bool loader = false;
   String loading = "No Data....";
+  List dummyData = [];
+  List name = [];
 
 
   Future<void> _selectDate(BuildContext context) async {
     formattedDate = formatter.format(currentDate);
     final DateTime? pickedDate = await showDatePicker(
         context: context,
-        builder: (context,child){
-          return Theme(data: ThemeData(
-            colorScheme: const ColorScheme.light(
-              primary: Colors.blue,
-              onPrimary: Colors.white,
-              surface: Colors.blue,
-              onSurface: Colors.black12,
-            ),
-            dialogBackgroundColor: Colors.white,
-          ),child: child ?? const Text(""));
+        builder: (context, child) {
+          return Theme(
+              data: ThemeData(
+                colorScheme: const ColorScheme.light(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                  surface: Colors.blue,
+                  onSurface: Colors.black12,
+                ),
+                dialogBackgroundColor: Colors.white,
+              ),
+              child: child ?? const Text(""));
         },
         initialDate: currentDate,
-        firstDate: DateTime(2020,1,1),
-        lastDate: DateTime(2050,1,1));
+        firstDate: DateTime(2020, 1, 1),
+        lastDate: DateTime(2050, 1, 1));
     if (pickedDate != null && pickedDate != currentDate) {
       setState(() {
         currentDate = pickedDate;
@@ -68,10 +71,10 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-
   fireData() async {
     //print("im at before atlast of firedata");
     databaseReference.once().then((value) async {
+      dummyData.clear();
       staffName.clear();
       from.clear();
       to.clear();
@@ -82,20 +85,33 @@ class _MyAppState extends State<MyApp> {
         loader = false;
       });
       dataJson = value.snapshot.value;
-      if(DateTime.now().isBefore(currentDate))
-      {
+      if (DateTime.now().isBefore(currentDate)) {
         setState(() {
           loading = "Please select a proper date";
         });
-      }else{
+      } else {
         for (var element in value.snapshot.children) {
+          // print(element.key);
+          personalData = element.value;
+          // staffName.add(personalData['name']);
+
           for (var ele in element.children) {
             if (ele.key == "workManager") {
               for (var el in ele.children) {
                 for (var e in el.children) {
                   if (e.key == combination.split(' ')[0]) {
+                    // print(e.value);
+                    // dummyData =
                     for (var val in e.children) {
                       data = val.value;
+                      print(data);
+                      print("----------------------------------------");
+                      if(data['name'] == personalData['name'])
+                      {
+                        print(data);
+                        dummyData.add(data);
+                        // print(dummyData);
+                      }
                       setState(() {
                         loader = true;
                         staffName.add(data['name']);
@@ -104,6 +120,17 @@ class _MyAppState extends State<MyApp> {
                         time_in_hours.add(data['time_in_hours']);
                         workDone.add(data['workDone']);
                         workPrecentage.add(data['workPercentage']);
+                        // print(staffName.toSet().toList());
+                        // Map<String, int> count = {};
+                        // for (var i in staffName) {
+                        //   count[i] = (count[i] ?? 0)+1;
+                        // }
+                        // print(count.toString());
+                        // print(to);
+                        // print(from);
+                        // print(time_in_hours);
+                        // print(workDone);
+                        // print(workPrecentage);
                       });
                     }
                   }
@@ -146,7 +173,6 @@ class _MyAppState extends State<MyApp> {
       // final json = value.snapshot.value as Map<dynamic, dynamic>;
       // final message = Message.fromJson(json);
 
-
       // final json = dataJson['staff'] as Map<dynamic, dynamic>;
       // print(json);
       // final message = Data.fromJson(json);
@@ -156,7 +182,6 @@ class _MyAppState extends State<MyApp> {
       //   {
       //     print(userdata);
       //   }
-
 
       // Map<dynamic, dynamic> res = _dataJson;
       // var x =[];
@@ -173,8 +198,22 @@ class _MyAppState extends State<MyApp> {
       //   print(i);
       //   // print(dataJson[]);
       // }
+
+      setState((){
+        name = staffName.toSet().toList();
+        // print(name);
+      });
+
+      // Map<String, int> count = {};
+      // for (var i in staffName) {
+      //   count[i] = (count[i] ?? 0)+1;
+      // }
+      // // print(count.toString());
+      //
+      // countvalues = count;
     });
   }
+
 
 
   @override
@@ -184,60 +223,121 @@ class _MyAppState extends State<MyApp> {
       child: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.orange,
-            title: combination.split(' ')[0] != " " ?Text("Date : ${combination.split(' ')[0]}"):const Text("WorkDone Data"),
+            title: combination.split(' ')[0] != " "
+                ? Text("Date : ${combination.split(' ')[0]}")
+                : const Text("WorkDone Data"),
             actions: [
               ElevatedButton(
                   style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.orange)
-                  ),
-                  onPressed: (){
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.orange)),
+                  onPressed: () {
                     combination = " ";
                     _selectDate(context);
                     // fireData();
                     // testData();
-                  }, child: const Text(" Get ",style: TextStyle(fontSize: 12),)),
+                  },
+                  child: const Text(
+                    " Get ",
+                    style: TextStyle(fontSize: 12),
+                  )),
             ],
           ),
-          body: loader ? GridView.builder(
-            itemCount: staffName.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 1.0,
-                mainAxisSpacing: 1.0),
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                  margin: const EdgeInsets.all(5.0),
-                  padding: const EdgeInsets.symmetric(horizontal: 13.0,vertical: 18.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(25.0),
-                    // border: Border.all(color: Colors.red)
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          body: loader ? ListView.builder(
+                  // itemCount: staffName.toSet().toList().length,
+                  itemCount: name.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                        margin: const EdgeInsets.all(5.0),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 13.0, vertical: 18.0),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade300,
+                          borderRadius: BorderRadius.circular(25.0),
+                          // border: Border.all(color: Colors.red)
+                        ),
+                        child: Column(
                           children: [
-                            Text(staffName[index],textAlign: TextAlign.left,style: const TextStyle(fontWeight: FontWeight.bold),),
-                            Text(workPrecentage[index]??" ")
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(name[index],textAlign: TextAlign.left,
+                                  style: const TextStyle(fontWeight: FontWeight.bold),),
+                                  ],
+                            ),
+                            SizedBox(
+                                            height: height*0.010,
+                                          ),
+                            ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: dummyData.length,
+                                itemBuilder: (BuildContext context, int ind) {
+                              return dummyData[ind]['name'].contains(name[index])? Card(
+                                color: Colors.white54,
+                                child: ListTile(
+                                  subtitle:Text("working hours: ${dummyData[ind]['time_in_hours']??" "}"),
+                                       leading: Column(
+                                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                         children: [
+                                           Text("${dummyData[ind]['from']??" "}"),
+                                           Text("To",style: TextStyle(fontSize: height*0.007),),
+                                           Text("${dummyData[ind]['to']??" "}"),
+                                         ],
+                                       ) ,
+                                       trailing: Text("${dummyData[ind]['workPercentage']??" "}"),
+                                       title: Text("${dummyData[ind]['workDone']??" "}",textAlign: TextAlign.left,),
+                                )
+                              ):Container();
+                            }),
                           ],
-                        ),
-                        SizedBox(
-                          height: height*0.010,
-                        ),
-                        Text("From : ${from[index]}",textAlign: TextAlign.left,),
-                        Text("To : ${to[index]}",textAlign: TextAlign.left,),
-                        Text("Time_in_hours : ${time_in_hours[index]}",textAlign: TextAlign.left,),
-                        Text("WorkDone : ${workDone[index]}",textAlign: TextAlign.left,),
-                      ],
-                    ),
-                  ));
-            },
-          ):Center(child: Text(loading),)
-      ),
+                        )
+                    );
+                  },
+                )
+              : Center(
+                  child: Text(loading),
+                )),
     );
   }
 }
 
+
+//
+// GridView.builder(
+//   itemCount: staffName.length,
+//   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//       crossAxisCount: 2,
+//       crossAxisSpacing: 1.0,
+//       mainAxisSpacing: 1.0),
+//   itemBuilder: (BuildContext context, int index) {
+//     return Container(
+//         margin: const EdgeInsets.all(5.0),
+//         padding: const EdgeInsets.symmetric(horizontal: 13.0,vertical: 18.0),
+//         decoration: BoxDecoration(
+//           color: Colors.grey.shade300,
+//           borderRadius: BorderRadius.circular(25.0),
+//           // border: Border.all(color: Colors.red)
+//         ),
+//         child: SingleChildScrollView(
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Text(staffName[index],textAlign: TextAlign.left,style: const TextStyle(fontWeight: FontWeight.bold),),
+//                   Text(workPrecentage[index]??" ")
+//                 ],
+//               ),
+//               SizedBox(
+//                 height: height*0.010,
+//               ),
+//               Text("From : ${from[index]}",textAlign: TextAlign.left,),
+//               Text("To : ${to[index]}",textAlign: TextAlign.left,),
+//               Text("Time_in_hours : ${time_in_hours[index]}",textAlign: TextAlign.left,),
+//               Text("WorkDone : ${workDone[index]}",textAlign: TextAlign.left,),
+//             ],
+//           ),
+//         ));
+//   },
+// )
